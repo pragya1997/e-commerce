3465116e4232717cd13ed34637b664fe31d53fcc
Pragya Marodia <pm.pragya1997@gmail.com>
2025-03-31 02:19:32 +0530

    first steps

--- src/test/java/com/microservices/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/microservices/ecommerce/EcommerceApplicationTests.java
@@ -3,11 +3,9 @@ package com.microservices.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class EcommerceApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
